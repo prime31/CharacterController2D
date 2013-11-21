e95bf7431bbf20811a6b05490d9fb225b5c1fa1d
Mike <mike@prime31.com>
2013-11-21 09:55:01 -0800

    minor cleanup for demo scene

--- Assets/Demo/AnimationTester.cs
+++ Assets/Demo/AnimationTester.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 
 
+/// <summary>
+/// This is a very basic class that skips all the Mecanim features and just plays animations based on input
+/// </summary>
 public class AnimationTester : MonoBehaviour
 {
 	Animator _animator;
@@ -18,22 +21,14 @@ public class AnimationTester : MonoBehaviour
 
 	void Update()
 	{
-		if( _controller != null )
-		{
-			if( _controller.isGrounded && ( Input.GetKey( KeyCode.LeftArrow ) || Input.GetKey( KeyCode.RightArrow ) ) )
-			{
-				_animator.goToStateIfNotAlreadyThere( Animator.StringToHash( "Base Layer.Run" ) );
-			}
-			else if( _controller.isGrounded )
-			{
-				_animator.goToStateIfNotAlreadyThere( Animator.StringToHash( "Base Layer.Idle" ) );
-			}
-		}
+		if( _controller.isGrounded && ( Input.GetKey( KeyCode.LeftArrow ) || Input.GetKey( KeyCode.RightArrow ) ) )
+			_animator.goToStateIfNotAlreadyThere( Animator.StringToHash( "Base Layer.Run" ) );
+		else if( _controller.isGrounded )
+			_animator.goToStateIfNotAlreadyThere( Animator.StringToHash( "Base Layer.Idle" ) );
+
 
 		if( Input.GetKeyDown( KeyCode.UpArrow ) )
 			_animator.Play( Animator.StringToHash( "Base Layer.Jump" ) );
-		if( Input.GetKeyDown( KeyCode.K ) )
-			_animator.Play( Animator.StringToHash( "Base Layer.Fall" ) );
 	}
 
 }
